77daeaeb2c8207384c44011e13711e025f73e0cb
axelzucho <axelzucho1@gmail.com>
2020-06-07 00:27:22 -0500

    refactor: include new files

--- src/JIT.hs
+++ src/JIT.hs
@@ -18,41 +18,41 @@ import LLVM.Analysis
 
 import qualified LLVM.ExecutionEngine as EE
 
-foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> (IO Double)
+foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> IO Double
 
 run :: FunPtr a -> IO Double
-run fn = haskFun (castFunPtr fn :: FunPtr (IO Double))
+run fnPtr = haskFun (castFunPtr fnPtr :: FunPtr (IO Double))
 
 jit :: Context -> (EE.MCJIT -> IO a) -> IO a
-jit c = EE.withMCJIT c optlevel model ptrelim fastins
+jit c = EE.withMCJIT c oLevel model framePtrElim fastIns
   where
-    optlevel = Just 0  -- optimization level
+    oLevel = Just 0  -- optimization level
     model    = Nothing -- code model ( Default )
-    ptrelim  = Nothing -- frame pointer elimination
-    fastins  = Nothing -- fast instruction selection
+    framePtrElim  = Nothing -- frame pointer elimination
+    fastIns  = Nothing -- fast instruction selection
 
-passes :: PassSetSpec
-passes = defaultCuratedPassSetSpec { optLevel = Just 3 }
+astPasses :: PassSetSpec
+astPasses = defaultCuratedPassSetSpec { optLevel = Just 3 }
 
 runJIT :: AST.Module -> IO (Either String AST.Module)
-runJIT mod = do
+runJIT mainModule = do
   withContext $ \context ->
     jit context $ \executionEngine ->
-      runExceptT $ withModuleFromAST context mod $ \m ->
-        withPassManager passes $ \pm -> do
+      runExceptT $ withModuleFromAST context mainModule $ \m ->
+        withPassManager astPasses $ \pm -> do
           -- Optimization Pass
           runPassManager pm m
-          optmod <- moduleAST m
+          optModule <- moduleAST m
           s <- moduleLLVMAssembly m
           putStrLn s
 
           EE.withModuleInEngine executionEngine m $ \ee -> do
-            mainfn <- EE.getFunction ee (AST.Name "main")
-            case mainfn of
+            mainFn <- EE.getFunction ee (AST.Name "main")
+            case mainFn of
               Just fn -> do
-                res <- run fn
-                putStrLn $ "Evaluated to: " ++ show res
+                ret <- run fn
+                putStrLn $ "Evaluated to: " ++ show ret
               Nothing -> return ()
 
           -- Return the optimized module
-          return optmod
+          return optModule
